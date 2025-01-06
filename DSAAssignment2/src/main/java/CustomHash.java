83ee3d99d737ca884bcef4eb2a6a5185b4f2b107
Sekhni <20105853@mail.wit.ie>
2025-01-06 14:31:03 +0000

    hello

--- DSAAssignment2/src/main/java/CustomHash.java
+++ DSAAssignment2/src/main/java/CustomHash.java
@@ -162,6 +162,8 @@ public class CustomHash {
 
 
 
+
+
         }
         System.out.println();
 
---
+++ DSAAssignment2/src/main/java/Recipe.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Recipe {
+    private Drink drink;
+    private Map<Ingredient, Double> ingredientQuantities; // Ingredient and its quantity in ml
+
+    public Recipe(Drink drink) {
+        this.drink = drink;
+        this.ingredientQuantities = new HashMap<>();
+    }
+
+    public void addIngredient(Ingredient ingredient, double quantity) {
+        ingredientQuantities.put(ingredient, quantity);
+    }
+
+    public void removeIngredient(Ingredient ingredient) {
+        ingredientQuantities.remove(ingredient);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "drink=" + drink +
+                ", ingredientQuantities=" + ingredientQuantities +
+                '}';
+    }
+
+}
